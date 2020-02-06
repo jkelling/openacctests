9e6e390134e42d6baedaaae3cb7e861a4a406aa6
Jeffrey Kelling <j.kelling@hzdr.de>
2020-02-06 18:10:14 +0100

    collectiveShared: Split output

--- collectiveShared/collectiveShared.cpp
+++ collectiveShared/collectiveShared.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <iostream>
 #include <vector>
 #include <cuda_runtime.h>
 
@@ -7,15 +8,15 @@ __device__ extern "C" void __syncthreads();
 
 int main()
 {
-	constexpr size_t nGangs = 1;
-	constexpr size_t nWorkers = 64;
+	constexpr size_t nGangs = 10;
+	constexpr size_t nWorkers = 512;
 
 	constexpr size_t N = 2*nGangs*nWorkers;
 	int io[N];
 	for(int a = 0; a < N; ++a)
 		io[a] = 1000000000;
 
-	for (int i = 0; i < 1; ++i)
+	for (int i = 0; i < 100; ++i)
 	{
 	#pragma acc parallel num_gangs(nGangs) num_workers(nWorkers) copy(io)
 	// #pragma acc parallel num_gangs(nGangs) vector_length(nWorkers) copy(io)
@@ -35,28 +36,37 @@ int main()
 				io[g*nWorkers+w] += w+1;
 
 				/* collective load/store */
-				cache[w] = io[(nGangs+g)*nWorkers+w];
-				__syncthreads();
-				// cache[(w+3)%nWorkers] += (w+3)%nWorkers+1;
-				cache[(w+3)%nWorkers] += w+1;
+				// load a pseudo-random number of times to threads out of sync and thusly provoke race conditions
+				for(int r = 0; r < ((unsigned int)((w+i*nWorkers)*nGangs+g)*1103515245u)/(double)UINT_MAX*13+1; ++r)
+					cache[w] = io[(nGangs+g)*nWorkers+w];
+				// __syncthreads();
+				cache[(w+3)%nWorkers] += (w+3)%nWorkers+1;
+				// cache[(w+3)%nWorkers] += w+1;
 				// printf("%lld %lld : %lld\n",g,w, cache[(w+3)%nWorkers]);
 				// printf("");
-				__syncthreads();
+				// __syncthreads();
 				io[(nGangs+g)*nWorkers+w] = cache[w];
 			}
 		}
 	}
 	}
 
-	for(int a = 0; a < 2; ++a)
+	int a = 0;
+	for(int g = 0; g < nGangs; ++g)
 	{
-		for(int g = 0; g < nGangs; ++g)
+		for(int w = 0; w < nWorkers; ++w)
 		{
-			for(int w = 0; w < nWorkers; ++w)
-			{
-				printf("%d\tg_%d\tw_%d:\t%d\n", a,g,w,io[(a*nGangs+g)*nWorkers+w]);
-			}
-			printf("\n");
+			printf("g_%d\tw_%d:\t%d\n", g,w,io[(a*nGangs+g)*nWorkers+w]);
 		}
+		printf("\n");
+	}
+	a = 1;
+	for(int g = 0; g < nGangs; ++g)
+	{
+		for(int w = 0; w < nWorkers; ++w)
+		{
+			fprintf(stderr, "g_%d\tw_%d:\t%d\n", g,w,io[(a*nGangs+g)*nWorkers+w]);
+		}
+		fprintf(stderr, "\n");
 	}
 }
