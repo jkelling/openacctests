3bf09ec55e142d3329e964fc2dca95d6210ec135
Jeffrey Kelling <j.kelling@hzdr.de>
2020-02-11 10:02:19 +0100

    hostClass: Add variatic template and lambda

--- hostClass/hostClass.cpp
+++ hostClass/hostClass.cpp
@@ -10,17 +10,13 @@ struct HostClass
 
 	int a;
 	template<typename T>
-	int operator() (const T &b) {return a+b;}
+	int operator() (const T &b) const {return a+b;}
 	int f(int b) {return a+b;}
 };
 
-int main()
+template<class T, class ...Args>
+void f(size_t nGangs, size_t nWorkers, T t, Args ...args)
 {
-	constexpr size_t nGangs = 2;
-	constexpr size_t nWorkers = 2;
-
-	HostClass<2> hostClass{5};
-
 	#pragma acc parallel num_gangs(nGangs) num_workers(nWorkers)
 	// #pragma acc parallel num_gangs(nGangs) vector_length(nWorkers)
 	{
@@ -32,8 +28,21 @@ int main()
 			// #pragma acc loop vector
 			for(int w = 0; w < nWorkers; ++w)
 			{
-				printf("%lld\n", hostClass(3));
+				[t](auto ...a)
+				{
+					printf("%lld\n", t(a...));
+				} (args...);
 			}
 		}
 	}
+}
+
+int main()
+{
+	constexpr size_t nGangs = 2;
+	constexpr size_t nWorkers = 2;
+
+	HostClass<2> hostClass{5};
+
+	f(nGangs, nWorkers, hostClass, 3);
 }
